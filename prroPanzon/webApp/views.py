65cc93f27e43cb4d9a513ec7752c3430fba00c0b
israncho <isragutierrez@ciencias.unam.mx>
2023-10-09 13:04:45 -0600

    vista mas segura para el login

--- prroPanzon/webApp/views.py
+++ prroPanzon/webApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
@@ -10,22 +11,26 @@ def index(request):
 
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        print(user)
-        if user is not None:
-            login(request, user)
-            return redirect('webApp:dashboard')
+        form = AuthenticationForm(request, request.POST)
+        print(form.is_valid())
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('webApp:dashboard')
         else:
             return render(request, 'login.html', {'error_message': 'Credenciales incorrectas'})
+    else:
+        form = AuthenticationForm()
 
-    return render(request, 'login.html')
+    return render(request, 'login.html', {'form': form})
 
 
 def logout_view(request):
     logout(request)
-    return redirect('webApp:login')
+    return redirect('webApp:index')
 
 
 @login_required
@@ -35,12 +40,10 @@ def dashboard_view(request):
     email = user.email
     registration_date = user.date_joined.strftime('%Y-%m-%d %H:%M:%S')
 
-    # Contexto para pasar datos al template
     contexto = {
         'nombre_de_usuario': user_name,
         'correo': email,
         'fecha_de_alta': registration_date,
     }
 
-    # Renderizar la plantilla con el contexto
     return render(request, 'dashboard.html', contexto)
