3c7f9f78aaa93295fb14231bab193d4cb7cbe5dd
israncho <isragutierrez@ciencias.unam.mx>
2023-10-08 23:26:38 -0600

    vistas para el login

--- prroPanzon/webApp/urls.py
+++ prroPanzon/webApp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-
 from . import views
 
+app_name = 'webApp'
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
 ]
+
--- prroPanzon/webApp/views.py
+++ prroPanzon/webApp/views.py
@@ -1,7 +1,46 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def index(request):
     return render(request, "index.html")
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect('webApp:dashboard')
+        else:
+            return render(request, 'login.html', {'error_message': 'Credenciales incorrectas'})
+
+    return render(request, 'login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('webApp:login')
+
+
+@login_required
+def dashboard_view(request):
+    user = request.user
+    user_name = user.username
+    email = user.email
+    registration_date = user.date_joined.strftime('%Y-%m-%d %H:%M:%S')
+
+    # Contexto para pasar datos al template
+    contexto = {
+        'nombre_de_usuario': user_name,
+        'correo': email,
+        'fecha_de_alta': registration_date,
+    }
+
+    # Renderizar la plantilla con el contexto
+    return render(request, 'dashboard.html', contexto)
