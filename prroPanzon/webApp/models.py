709427437b120e44f46ba2f3cb25d7e2765dc6f0
israncho <isragutierrez@ciencias.unam.mx>
2023-10-08 23:25:52 -0600

    modelo para los usuarios

--- prroPanzon/webApp/models.py
+++ prroPanzon/webApp/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class AppUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
+
+
+# Cambiar los nombres de acceso inverso en las relaciones
+
+AppUser._meta.get_field('groups').remote_field.related_name = 'appuser_set'
+AppUser._meta.get_field('user_permissions').remote_field.related_name = 'appuser_set'
+
